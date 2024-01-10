5a89c1e89bd5f87089aca6788d09c373f66005ed
Esma Sude Karakuş <sudekarakus@outlook.com.tr>
2024-01-10 15:01:20 +0300

    lists initialized

--- Domain/Entities/Menu.cs
+++ Domain/Entities/Menu.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -9,6 +10,12 @@ namespace Domain.Entities
 {
     public class Menu : Product_BaseEntity
     {
+        public Menu()
+        {
+            Condiments= new List<Condiment>();
+            Burgers= new List<Burger>();
+            Beverages= new List<Beverage>();
+        }
         public Size Size { get; set; }
         public ICollection<Condiment> Condiments { get; set; }
         public ICollection<Burger> Burgers { get; set;}
