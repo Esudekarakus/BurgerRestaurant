98df157f245ad3478577cd5359111c8a56360cf2
Tarkovski7 <trkbgr1618@gmail.com>
2024-01-10 17:41:25 +0300

    Configuration context oluşturuldu

---
+++ DAL/Context/AppDbContext.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Domain.Entities;
+using DAL.Configurations;
+
+namespace DAL.Context
+{
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Beverage> Beverages { get; set; }
+        public DbSet<Burger> Burgers { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Condiment> Condiments { get; set; }
+        public DbSet<Menu> Menu { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder
+            .ApplyConfiguration(new BeverageConfiguration())
+            .ApplyConfiguration(new BurgerConfiguration())
+            .ApplyConfiguration(new CondimentConfiguration());
+            base.OnModelCreating(builder);
+        }
+
+
+    }
+}
