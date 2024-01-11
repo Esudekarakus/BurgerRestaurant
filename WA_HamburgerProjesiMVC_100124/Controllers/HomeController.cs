6715ea744fe3d375841d63fde502241a576948ba
Shohzod İrgaş <131816386+SahzodDev@users.noreply.github.com>
2024-01-11 21:05:55 +0300

    dsgsgd

--- WA_HamburgerProjesiMVC_100124/Controllers/HomeController.cs
+++ WA_HamburgerProjesiMVC_100124/Controllers/HomeController.cs
@@ -1,37 +1,47 @@
-﻿using Domain.Entities;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using WA_HamburgerProjesiMVC_100124.Models;
 
 namespace WA_HamburgerProjesiMVC_100124.Controllers
 {
-	public class HomeController : Controller
-	{
-		private readonly ILogger<HomeController> _logger;
-		
-		public HomeController(ILogger<HomeController> logger)
-		{
-			_logger = logger;
-		}
+    // authorize
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> logger;
+
+        // Layout olacak 
+        // Navbar Profil  -  Burger Menu (Çizgi)  -  Search butonu  -  
+        // Profil (Bilgiler  -  Önceki siparişler  -  Sepetim  -  çıkış )
+        // Hamburger Menu (Ana sayfa  -  Hakkımızda  -  İletişim  -  Sipariş ver  )
+        // Uygulama açıldığında giriş ekranı karşılasın. Giriş yapmadan devam edilmesin.
+
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            this.logger = logger;
+        }
 
         public IActionResult Index()
-		{
-			
-			
-			return View();
-		}
+        {
+            return View();
+        }
 
-		public IActionResult Privacy()
-		{
-			return View();
-		}
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Order()
+        {
+            return View();
+        }
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
-	}
+    }
 }
