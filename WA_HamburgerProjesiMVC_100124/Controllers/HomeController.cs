c9b6d1d8db21106fbdbaf67b5ff22915143c0942
Tarkovski7 <trkbgr1618@gmail.com>
2024-01-11 15:38:04 +0300

    home update

--- WA_HamburgerProjesiMVC_100124/Controllers/HomeController.cs
+++ WA_HamburgerProjesiMVC_100124/Controllers/HomeController.cs
@@ -4,13 +4,19 @@ using WA_HamburgerProjesiMVC_100124.Models;
 
 namespace WA_HamburgerProjesiMVC_100124.Controllers
 {
+	// authorize
 	public class HomeController : Controller
 	{
-		private readonly ILogger<HomeController> _logger;
+		// Layout olacak 
+		// Navbar Profil  -  Burger Menu (Çizgi)  -  Search butonu  -  
+		// Profil (Bilgiler  -  Önceki siparişler  -  Sepetim  -  çıkış )
+		// Hamburger Menu (Ana sayfa  -  Hakkımızda  -  İletişim  -  Sipariş ver  )
+		// Uygulama açıldığında giriş ekranı karşılasın. Giriş yapmadan devam edilmesin.
 
-		public HomeController(ILogger<HomeController> logger)
+
+		public HomeController()
 		{
-			_logger = logger;
+			
 		}
 
 		public IActionResult Index()
@@ -23,10 +29,6 @@ namespace WA_HamburgerProjesiMVC_100124.Controllers
 			return View();
 		}
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
+		
 	}
 }
