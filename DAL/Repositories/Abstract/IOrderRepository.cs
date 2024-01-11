e987be3cfff0aa64c496c63704487c6f1705838e
Harun Azizoglu <117512095+harxite@users.noreply.github.com>
2024-01-11 13:54:22 +0300

    Order repository completed

---
+++ DAL/Repositories/Abstract/IOrderRepository.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories.Abstract
+{
+    public interface IOrderRepository : IRepository<Order>
+    {
+        IEnumerable<Order> GetAllByUserId(string id);
+        Order GetByIdIncludeMenus(int id);
+        Order GetByUserIdIncludeMenus(string id);
+
+        IEnumerable<Order> GetAllOrderByCreatedDate();
+        IEnumerable<Order> GetByUserIdOrderByCreatedDate(string id);
+        IEnumerable<Order> GetByUserIdIncludeMenusOrderByCreatedDate(string id);
+
+    }
+}
