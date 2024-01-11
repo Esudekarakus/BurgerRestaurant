84971a919a903a1c90852f65f6a624943cd7f7eb
Harun Azizoglu <117512095+harxite@users.noreply.github.com>
2024-01-11 21:25:20 +0300

    admin

--- WA_HamburgerProjesiMVC_100124/Controllers/AdminController.cs
+++ WA_HamburgerProjesiMVC_100124/Controllers/AdminController.cs
@@ -4,7 +4,12 @@ namespace WA_HamburgerProjesiMVC_100124.Controllers
 {
 	public class AdminController : Controller
 	{
-		public IActionResult Index()
+        // Layout olusturulacak ( Side bar + header + footer)
+        // Viewlar layoutun icinde main kismina RenderBody ile gelecek.
+        // Header kisminda giris yapan adminin ismi + logout + bildirim butonu + mesaj butonu  olacak.
+        // SideBar'da Restorana ait istatistikler + Menu crud + Urun crud + Kategori crud + Kullanicilari goruntuleme + Siparisleri goruntuleme + Mesajlari goruntuleme 
+
+        public IActionResult Index()
 		{
 			return View();
 		}
