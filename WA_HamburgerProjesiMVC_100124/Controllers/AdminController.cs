24177a6b9fbd1eb48377b0a8dd10293bae87be32
Harun Azizoglu <117512095+harxite@users.noreply.github.com>
2024-01-12 10:48:11 +0300

    admin controller

--- WA_HamburgerProjesiMVC_100124/Controllers/AdminController.cs
+++ WA_HamburgerProjesiMVC_100124/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WA_HamburgerProjesiMVC_100124.Controllers
 {
@@ -13,7 +14,184 @@ namespace WA_HamburgerProjesiMVC_100124.Controllers
 		{
 			return View();
 		}
+        public IActionResult Dashboard()
+        {
+            // Toplam kullanici sayisi
+            // Ekstra urunlerden edilen toplam ciro
+            // Gunluk alinan siparis grafigi
+            // Alinan toplam siparis miktari
+            // Ciro grafigi
+            // Toplam ciro
+            // Toplam menu miktari
+            // Toplam urun miktari
 
-		
-	}
+            return View();
+        }
+        public IActionResult Menus()
+        {
+            // Butun menulerin listesi 
+            // Menu ekle butonu 
+            // Update butonu 
+            // Delete butonu
+
+            return View();
+        }
+        public IActionResult CreateMenu()
+        {
+            // Menu ekleme ekrani
+            // Menuye ait bilgilerin girildigi form
+            // Menuye ait fotografin eklenebildigi + goruntulendigi alan
+            // Ekle butonu
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateMenu(Menu menu)
+        {
+            // Servisten metot cagirip menu databaseye eklenecek. 
+
+            return View();
+        }
+        public IActionResult UpdateMenu(int id)
+        {
+            // Menu update ekrani
+            // Menuye ait bilgilerin geldigi ve degisikligin yapilacagi form
+            // Menuye ait fotografin  goruntulendigi + degistirilebildigi alan
+            // Guncelle butonu
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult UpdateMenu(Menu menu)
+        {
+            // Servisten metot cagirip databaseden gelen menu guncellenip tekrar databaseye gonderilecek.
+            
+            return View();
+        }
+        public IActionResult DeleteMenu(int id)
+        {
+            // Servisten metot cagirip databaseden gelen menu silinecek.
+
+            return View();
+        }
+        public IActionResult Products()
+        {
+
+            // Butun urunlerin listesi 
+            // Urun ekle butonu 
+            // Update butonu 
+            // Delete butonu
+
+            return View();
+        }
+        public IActionResult CreateProduct()
+        {
+            // Urun ekleme ekrani
+            // Urune ait bilgilerin girildigi form
+            // Urune ait fotografin eklenebildigi + goruntulendigi alan
+            // Ekle butonu
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateProduct(Product product)
+        {
+            // Servisten metot cagirip product databaseye eklenecek. 
+
+            return View();
+        }
+        public IActionResult UpdateProduct(int id)
+        {
+            // Urun update ekrani
+            // Urune ait bilgilerin geldigi ve degisikligin yapilacagi form
+            // Urune ait fotografin  goruntulendigi + degistirilebildigi alan
+            // Guncelle butonu
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult UpdateProduct(Product product)
+        {
+            // Servisten metot cagirip databaseden gelen urun guncellenip tekrar databaseye gonderilecek.
+
+            return View();
+        }
+        public IActionResult DeleteProduct(int id)
+        {
+            // Servisten metot cagirip databaseden gelen product silinecek.
+
+            return View();
+        }
+        public IActionResult Categories()
+        {
+            // Butun kategorilerin listesi 
+            // Kategori ekle butonu 
+            // Update butonu 
+            // Delete butonu
+
+            return View();
+        }
+        public IActionResult CreateCategory()
+        {
+            // Kategori ekleme ekrani
+            // Kategoriye ait bilgilerin girildigi form
+            // Ekle butonu
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateCategory(Category category)
+        {
+            // Servisten metot cagirip kategori databaseye eklenecek. 
+
+            return View();
+        }
+        public IActionResult UpdateCategory(int id)
+        {
+            // Kategori update ekrani
+            // Kategoriye ait bilgilerin geldigi ve degisikligin yapilacagi form
+            // Guncelle butonu
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult UpdateCategory(Category category)
+        {
+            // Servisten metot cagirip databaseden gelen kategori guncellenip tekrar databaseye gonderilecek.
+
+            return View();
+        }
+        public IActionResult DeleteCategory(int id)
+        {
+            // Servisten metot cagirip databaseden gelen product silinecek.
+
+            return View();
+        }
+        public IActionResult Users()
+        {
+            // Butun kullanicilarin listesi
+            // Kullanicilarin durumu -- aktif/pasif e cekilebilecek
+            // Kullanicinin kayit olusturma tarihi 
+            // Her bir kullanicinin toplam siparis miktari + toplam yaptigi odeme 
+            // Toplam kullanici sayisi
+
+            return View();
+        }
+        public IActionResult Orders()
+        {
+            // Butun siparislerin goruntulendigi liste 
+            // Siparis id + siparis tarihi + siparis edilen urunler + kullanici ismi + fiyat + siparis durumu
+            // Siparisin iptal edilebilecegi bir iptal butonu (opsiyonel)
+            return View();
+        }
+        public IActionResult Messages()
+        {
+            // Kullanicilardan gelen mesajlarin goruntulenebildigi ekran 
+
+            return View();
+        }
+    }
 }
