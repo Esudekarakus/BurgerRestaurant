f44a2592a691bec9c80cc381338ef07e5b58150e
Shohzod İrgaş <131816386+SahzodDev@users.noreply.github.com>
2024-01-16 16:23:20 +0300

    search logic update

---
+++ WA_HamburgerProjesiMVC_100124/wwwroot/js/LayoutScripts.js
@@ -0,0 +1,21 @@
+﻿function SearchItems() {
+    $(document).ready(function () {
+        $('#searchButton').click(function () {
+            var searchTerm = $('#searchInput').val().toLowerCase();
+
+            // Log the search term to the console
+            console.log('Search Term: ' + searchTerm);
+
+            // Hide or show cards based on the case-insensitive search term
+            $('.card').each(function () {
+                var cardSearchTerm = $(this).data('search');
+                console.log('Card Search Term: ' + cardSearchTerm);
+                if (cardSearchTerm && cardSearchTerm.includes(searchTerm)) {
+                    $(this).show();
+                } else {
+                    $(this).hide();
+                }
+            });
+        });
+    });
+}
--- WA_HamburgerProjesiMVC_100124/wwwroot/js/script.js
+++ WA_HamburgerProjesiMVC_100124/wwwroot/js/script.js
@@ -78,3 +78,4 @@ function GetProducts() {
     });
 
 }
+
