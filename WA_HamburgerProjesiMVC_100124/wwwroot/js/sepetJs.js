50604d51d89cad5fa88829bdb67952f5b7c7ff99
Esma Sude Karakuş <sudekarakus@outlook.com.tr>
2024-01-16 20:58:27 +0300

    js partial view

---
+++ WA_HamburgerProjesiMVC_100124/wwwroot/js/sepetJs.js
@@ -0,0 +1,13 @@
+﻿function GetShoppingCard() {
+    $.ajax({
+        url: "/Home/GetShoppingCard",
+        type: "get",
+        success: function (response) {
+            $("#container").html(response);
+        },
+        error: function () {
+            $("#error").html("<h3>Something went wrong</h3>");
+        }
+    });
+
+}
