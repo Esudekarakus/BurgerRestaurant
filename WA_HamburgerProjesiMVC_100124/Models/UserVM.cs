f2e2a5229b39f5e1be0efc7c62d90d00de909d63
Shohzod İrgaş <131816386+SahzodDev@users.noreply.github.com>
2024-01-21 20:08:49 +0300

    fixed profile section

--- WA_HamburgerProjesiMVC_100124/Models/UserVM.cs
+++ WA_HamburgerProjesiMVC_100124/Models/UserVM.cs
@@ -7,11 +7,13 @@ namespace WA_HamburgerProjesiMVC_100124.Models
 {
     public class UserVM
     {
+        public string Id { get; set; }
+        public string UserName { get; set; }
         public string Name { get; set; }
         public string SurName { get; set; }
         public string EMail { get; set; }
-        public string Address { get; set; }
-        public string Password { get; set; }
+        
+        
         
         
     }
